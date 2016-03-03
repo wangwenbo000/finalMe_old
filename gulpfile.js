1ba7861db11e468b1b973c4d6c7793fff6bdc9d8
wangwenbo <wangwenbo000@me.com>
2016-03-03 18:06:33 +0800

    细节调整

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,41 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var minifyCSS = require('gulp-minify-css');
-var minifyejs = require('gulp-minify-ejs');
-var replace = require('gulp-replace');
-var md5 = require('md5');
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var rename=require('gulp-rename');
+var minifyCSS=require('gulp-minify-css');
+var minifyejs=require('gulp-minify-ejs');
+var replace=require('gulp-replace');
+var md5=require('md5');
+var htmloptimize=require('gulp-htmloptimize');
 
-gulp.task('sass', function () {
+gulp.task('sass', function(){
   return gulp.src('./DaVinci/sass/style.scss')
-      .pipe(rename("index_style.css"))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(rename("index_style.css"))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest('./www/static/css/'));
+    .pipe(rename("index_style.css"))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename("index_style.css"))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./www/static/css/'));
 });
-gulp.task('home', function () {
+gulp.task('home', function(){
   return gulp.src('./DaVinci/home/*.html')
-      .pipe(minifyejs())
-      .pipe(gulp.dest('./view/home/'))
+    .pipe(minifyejs())
+    .pipe(htmloptimize({}))
+    .pipe(gulp.dest('./view/home/'))
 });
-gulp.task('home-a', function () {
+gulp.task('home-a', function(){
   return gulp.src('./DaVinci/article/*.html')
-      .pipe(minifyejs())
-      .pipe(gulp.dest('./view/home/article/'))
+    .pipe(minifyejs())
+    .pipe(htmloptimize({}))
+    .pipe(gulp.dest('./view/home/article/'))
 });
-gulp.task('home-p', function () {
+gulp.task('home-p', function(){
   return gulp.src('./DaVinci/public/*.html')
-      .pipe(minifyejs())
-      .pipe(replace('CHANGE_VALUE_NO', 'WWB_'+md5(new Date().getTime())))
-      .pipe(gulp.dest('./view/home/public/'))
+    .pipe(minifyejs())
+    //.pipe(htmloptimize({}))
+    .pipe(replace('CHANGE_VALUE_NO', 'WWB_' + md5(new Date().getTime())))
+    .pipe(gulp.dest('./view/home/public/'))
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function(){
   gulp.watch('./DaVinci/sass/style.scss', ['sass']);
   gulp.watch('./DaVinci/home/*.html', ['home']);
   gulp.watch('./DaVinci/article/*.html', ['home-a']);
